cec202d897fe089def06290141c45e41b753303c
SHNN267 <shnnhussie@gmail.com>
2026-06-20 15:26:01 +0300

    add elcetro folder

---
+++ active_directory/StoreInReg.ps1
@@ -0,0 +1,12 @@
+﻿$scriptFile = "C:\Scripts\CreateUser.ps1"
+$regPath = "HKLM:\Software\MyScripts"
+
+$scriptContent = Get-Content -Path $scriptFile -Raw
+
+if (-not (Test-Path $regPath)) {
+    New-Item -Path $regPath -Force | Out-Null
+}
+
+Set-ItemProperty -Path $regPath -Name "StoredScript" -Value $scriptContent
+
+Write-Output "Sorted in Reg has Done : - )"
